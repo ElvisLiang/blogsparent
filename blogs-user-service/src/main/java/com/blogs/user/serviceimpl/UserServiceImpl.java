fe6f70608917e9caac92cdb63896e25e1f87022b
Elvis <1904313468@qq.com>
2019-01-16 20:16:07 +0800

    添加用户详细信息查询

--- blogs-user-service/src/main/java/com/blogs/user/serviceimpl/UserServiceImpl.java
+++ blogs-user-service/src/main/java/com/blogs/user/serviceimpl/UserServiceImpl.java
@@ -1,17 +1,31 @@
 package com.blogs.user.serviceimpl;
 
+import com.blogs.user.dao.AuthorityMapper;
+import com.blogs.user.dao.RoleMapper;
+import com.blogs.user.dao.UserMapper;
 import com.blogs.user.entity.Authority;
 import com.blogs.user.entity.Role;
 import com.blogs.user.entity.User;
 import com.blogs.user.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
 
+    @Autowired
+    UserMapper userMapper;
+
+    @Autowired
+    RoleMapper roleMapper;
+
+    @Autowired
+    AuthorityMapper authorityMapper;
+
+
     @Override
     public User searchUserByUsername(String username) {
-
+        userMapper.selectByUsername(username);
         return null;
     }
 
